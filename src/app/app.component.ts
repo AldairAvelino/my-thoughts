27728f679ce8e411d5f1f42b733ca1aa6f5e25e5
Aldair Avelino <aldair03avelino@gmail.com>
2024-03-29 09:16:57 +0100

    Add more imports - AA

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './componentes/header/header.component';
+import { FooterComponent } from './componentes/footer/footer.component';
+import { CriarPensamentoComponent } from './componentes/pensamentos/criar-pensamento/criar-pensamento.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, CriarPensamentoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })

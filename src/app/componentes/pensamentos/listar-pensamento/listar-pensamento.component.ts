516718889321fc5d8f3124d70f5355e50545a758
Aldair Avelino <aldair03avelino@gmail.com>
2024-04-01 23:22:45 +0100

    Import CommonModule to work with *ngFor, and create a simple object that contains some pensamentos - AA

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -1,13 +1,32 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { PensamentoComponent } from '../pensamento/pensamento.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-listar-pensamento',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, PensamentoComponent, CommonModule],
   templateUrl: './listar-pensamento.component.html',
   styleUrl: './listar-pensamento.component.css'
 })
 export class ListarPensamentoComponent {
+  listaPensamento = [
+    {
+      conteudo: 'Conteudo 1',
+      autoria: 'Aldair',
+      modelo: 'modelo1'
+    },
+    {
+      conteudo: 'Conteudo 2',
+      autoria: 'Aldair',
+      modelo: 'modelo2'
+    },
+    {
+      conteudo: 'Conteudo 3',
+      autoria: 'Aldair',
+      modelo: 'modelo3'
+    }
+  ]
 
 }

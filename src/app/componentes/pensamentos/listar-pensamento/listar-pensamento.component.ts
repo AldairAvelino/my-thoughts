020b3cbb131271a45026699ee01d9d6d6249b400
Aldair Avelino <aldair03avelino@gmail.com>
2024-04-01 08:46:28 +0100

    Create the componente and its navegation -AA

---
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-listar-pensamento',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './listar-pensamento.component.html',
+  styleUrl: './listar-pensamento.component.css'
+})
+export class ListarPensamentoComponent {
+
+}

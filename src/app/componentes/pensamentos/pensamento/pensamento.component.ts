006921620522f0b6d1ec7abe8307f848f7f1e555
Aldair Avelino <aldair03avelino@gmail.com>
2024-04-01 22:20:00 +0100

    Create pensamento component and import its selector - AA

---
+++ src/app/componentes/pensamentos/pensamento/pensamento.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pensamento',
+  standalone: true,
+  imports: [],
+  templateUrl: './pensamento.component.html',
+  styleUrl: './pensamento.component.css'
+})
+export class PensamentoComponent {
+  pensamento = {
+    conteudo: 'Conteudo Teste',
+    autoria: 'Aldair',
+    modelo: 'modelo2'
+  }
+
+}

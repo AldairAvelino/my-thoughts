5ea5e303cd86deddfbdf133800044b7a03fd199d
Aldair Avelino <aldair03avelino@gmail.com>
2024-04-01 08:48:12 +0100

    Configure routes file - AA

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
+import { CriarPensamentoComponent } from './componentes/pensamentos/criar-pensamento/criar-pensamento.component';
+import { ListarPensamentoComponent } from './componentes/pensamentos/listar-pensamento/listar-pensamento.component';
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    component: ListarPensamentoComponent,
+    title: 'Principal'
+  },
+  {
+    path: 'listar-pensamento',
+    component: ListarPensamentoComponent,
+    title: 'Listar Pensamento'
+  },
+  {
+    path: 'criar-pensamento',
+    component: CriarPensamentoComponent,
+    title: 'Criar Pensamento'
+  }
+];

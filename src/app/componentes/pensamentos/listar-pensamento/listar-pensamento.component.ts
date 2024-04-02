75bff23ec58e4cc03a10e98b7af0b2a1e866e27a
Aldair Avelino <aldair03avelino@gmail.com>
2024-04-02 12:15:12 +0100

    Call for Pensamento interface array - AA

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { PensamentoComponent } from '../pensamento/pensamento.component';
 import { CommonModule } from '@angular/common';
+import { Pensamento } from '../pensamento/pensamento';
 
 @Component({
   selector: 'app-listar-pensamento',
@@ -11,22 +12,6 @@ import { CommonModule } from '@angular/common';
   styleUrl: './listar-pensamento.component.css'
 })
 export class ListarPensamentoComponent {
-  listaPensamento = [
-    {
-      conteudo: 'Conteudo 1 Conteudo 1Conteudo 1Conteudo 1Conteudo 1Conteudo 1Conteudo 1',
-      autoria: 'Aldair',
-      modelo: 'modelo1'
-    },
-    {
-      conteudo: 'Conteudo 2 Conteudo 2 Conteudo 2 Conteudo 2 Conteudo 2 Conteudo 2 Conteudo 2',
-      autoria: 'Aldair',
-      modelo: 'modelo2'
-    },
-    {
-      conteudo: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore, incidunt itaque nesciunt voluptatem atque sunt at laborum laudantium qui mollitia cupiditate numquam similique minus. Expedita quis quasi mollitia maiores veritatis.',
-      autoria: 'Aldair',
-      modelo: 'modelo3'
-    }
-  ]
+  listaPensamento: Pensamento [] = [] //Vai pegar dados do backend
 
 }

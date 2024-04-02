3f842b0cd69db1b67f3e68cab4e840bc6c239326
Aldair Avelino <aldair03avelino@gmail.com>
2024-04-02 10:53:35 +0100

    Add bigger message for conteudo - AA

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -12,21 +12,21 @@ import { CommonModule } from '@angular/common';
 })
 export class ListarPensamentoComponent {
   listaPensamento = [
-    // {
-    //   conteudo: 'Conteudo 1',
-    //   autoria: 'Aldair',
-    //   modelo: 'modelo1'
-    // },
-    // {
-    //   conteudo: 'Conteudo 2',
-    //   autoria: 'Aldair',
-    //   modelo: 'modelo2'
-    // },
-    // {
-    //   conteudo: 'Conteudo 3',
-    //   autoria: 'Aldair',
-    //   modelo: 'modelo3'
-    // }
+    {
+      conteudo: 'Conteudo 1 Conteudo 1Conteudo 1Conteudo 1Conteudo 1Conteudo 1Conteudo 1',
+      autoria: 'Aldair',
+      modelo: 'modelo1'
+    },
+    {
+      conteudo: 'Conteudo 2 Conteudo 2 Conteudo 2 Conteudo 2 Conteudo 2 Conteudo 2 Conteudo 2',
+      autoria: 'Aldair',
+      modelo: 'modelo2'
+    },
+    {
+      conteudo: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore, incidunt itaque nesciunt voluptatem atque sunt at laborum laudantium qui mollitia cupiditate numquam similique minus. Expedita quis quasi mollitia maiores veritatis.',
+      autoria: 'Aldair',
+      modelo: 'modelo3'
+    }
   ]
 
 }

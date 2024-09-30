2381e819e49e9737f12645b1d3a1eedde7577ba3
Aldair Avelino <71715517+AldairAvelino@users.noreply.github.com>
2024-09-30 15:52:50 +0100

    Update pensamento.service.spec.ts

--- src/app/componentes/pensamentos/pensamento.service.spec.ts
+++ src/app/componentes/pensamentos/pensamento.service.spec.ts
@@ -1,16 +1,1 @@
-import { TestBed } from '@angular/core/testing';
 
-import { PensamentoService } from './pensamento.service';
-
-describe('PensamentoService', () => {
-  let service: PensamentoService;
-
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(PensamentoService);
-  });
-
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
-});
